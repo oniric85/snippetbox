fdeed0f667c3c0e831e94f3aa49c84994e403467
Stefano Angaran <stefano@elearnsecurity.com>
2020-05-28 19:11:30 +0200

    Read latest snippets from db

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,6 +15,17 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	s, err := app.snippets.Latest()
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
+	}
+
 	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",

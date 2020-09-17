32f86a87766cee64fc403a6025577da0fcf3aa67
Stefano Angaran <stefano@elearnsecurity.com>
2020-09-17 23:48:42 +0200

    Added code to insert user

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -2,8 +2,11 @@ package mysql
 
 import (
 	"database/sql"
+	"strings"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/oniric85/snippetbox/pkg/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserModel struct {
@@ -11,6 +14,23 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+	VALUES(?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			if mysqlErr.Number == 1062 && strings.Contains(mysqlErr.Message, "users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+	}
+
 	return nil
 }
 
